1674afff2214ef1046be69a17b732bf76d7a45ae
adan291 <adanx05@gmail.com>
2015-10-09 13:52:32 +0200

    metodo funny y movimiento nuevo

--- Picture.java
+++ Picture.java
@@ -17,6 +17,10 @@ public class Picture
     private Circle sun;
     private Square campo;
     private Person pepe;
+    private Circle ojo1;
+    private Circle ojo2;
+    private Square boca;
+   
     
    
 
@@ -72,7 +76,23 @@ public class Picture
         pepe.moveVertical(20);
         pepe.makeVisible();
         
+        ojo1 = new Circle();
+        ojo1.changeColor("blue");
+        ojo1.moveHorizontal(-220);
+        ojo1.moveVertical(0);
+        ojo1.changeSize(30);
         
+        ojo2 = new Circle();
+        ojo2.changeColor("green");
+        ojo2.moveHorizontal(-160);
+        ojo2.moveVertical(0);
+        ojo2.changeSize(30);
+        
+        boca = new Square();
+        boca.changeColor("magenta");
+        boca.changeSize(20);
+        boca.moveHorizontal(-265);
+        boca.moveVertical(30);
         
          
         
@@ -115,14 +135,186 @@ public class Picture
     {
         if (sun != null)   // only if it's painted already...
         {
-            pepe.slowMoveHorizontal(75);
-            sun.slowMoveVertical(120);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            pepe.slowMoveHorizontal(1);
+            sun.slowMoveVertical(2);
+            
             wall.changeColor("black");
             window.changeColor("white");
             roof.changeColor("black");
             sun.changeColor("black");
             campo.changeColor("black");
             pepe.changeColor("black");
+            ojo1.changeColor("black");
+            ojo2.changeColor("black");
+            boca.changeColor("black");
+        }
+    }
+    /**
+     * Change this picture to use funny display
+     */
+    public void funny()
+    {
+        if (wall != null)   // only if it's painted already...
+        {
+            ojo1.makeVisible();
+            ojo2.makeVisible();
+            boca.makeVisible();
+            ojo1.moveHorizontal(305);
+            ojo2.moveHorizontal(320);
+            boca.moveHorizontal(315);
+            ojo1.moveVertical(-20);
+            ojo2.moveVertical(-20);
+            boca.moveVertical(-30);
+            
+            
+            
         }
     }
 }
